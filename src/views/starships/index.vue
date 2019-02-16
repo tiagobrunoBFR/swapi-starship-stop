8cd3a402a21e0c997d201ec3f9c981899dc2858b
tiago <tiagobrunobfr@gmail.com>
2019-02-16 12:26:39 -0300

    initial commit add vuetify

---
+++ src/views/starships/index.vue
@@ -0,0 +1,38 @@
+<template>
+
+        <v-container grid-list-md text-xs-center>
+        <v-text-field
+                label="Buscar Espaçonave"
+                prepend-inner-icon="search"
+                @keyup="keydownSearch"
+                v-model="search_starships"
+        ></v-text-field>
+        </v-container>
+
+
+
+</template>
+
+<script>
+    export default {
+        name: "index",
+        data(){
+            return {
+                search_starships:''
+            }
+        },
+
+        methods:{
+
+            keydownSearch()
+            {
+               return console.warn(this.search_starships)
+            }
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
